231190e0b76f57cc1bd0581cb6c05971177ec188
nobodyzxc <nobodyzxc.tw@gmail.com>
2021-01-29 18:59:45 +0000

    update

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import pandas as pd
 from motc_data.motc_data import *
 import pymongo
 import random
-#from ckiptagger import WS
 from fuzzywuzzy import fuzz, process
 from flask import (
     Flask,
@@ -36,7 +35,7 @@ bootstrap = Bootstrap(app)
 
 HOST = None
 SYSTEM_HOST = 'http://localhost:8508'
-#ws = WS("./data")
+
 
 if __name__ == '__main__':
 
@@ -51,6 +50,8 @@ if __name__ == '__main__':
     port = int(os.environ.get("BOTPORT", options.port))
 
     if not options.nobot:
+        from ckiptagger import WS
+        ws = WS("./data")
         # get channel_secret and channel_access_token from your environment variable
         channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
         channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
@@ -167,49 +168,50 @@ if not options.nobot:
             )
 loc_table = ["基隆端", "基隆", "八堵", "大華系統", "五堵", "汐止", "汐止系統", "高架汐止端", "東湖", "內湖", "圓山", "台北", "三重", "五股轉接道", "五股", "高公局", "泰山轉接道", "林口", "桃園", "機場系統", "中壢服務區", "內壢", "中壢轉接一", "中壢轉接二", "中壢", "平鎮系統", "幼獅", "楊梅", "高架楊梅端", "湖口", "湖口服務區", "竹北", "新竹", "新竹系統", "頭份", "頭屋", "苗栗", "銅鑼", "三義", "泰安服務區", "后里", "台中系統", "豐原", "大雅", "台中", "南屯", "王田", "彰化系統", "彰化", "埔鹽系統", "員林", "北斗", "西螺服務區", "西螺", "虎尾", "斗南", "雲林系統", "大林", "民雄", "嘉義", "水上", "嘉義系統", "新營服務區", "新營", "下營系統", "麻豆", "安定", "台南系統", "永康", "大灣", "仁德", "仁德系統", "仁德服務區", "路竹", "高科", "岡山", "楠梓", "鼎金系統", "高雄", "瑞隆路", "五甲系統", "五甲", "中山四路", "漁港路", "高雄端"]
 def parse_str(text):
-    return None
-#    data = ws([text])[0]
-#
-#    label = {"type":"","date":"","time":"","loc":"","loc_from":"","loc_to":"","Error":[]}
-#    if any([("申報" in d or "report" in d) for d in data]):
-#        label["type"] = "report"
-#        loc_list = []
-#        for i in range(len(data)):
-#            if ":" in data[i]: # time
-#                label = parse_time(data,i,label)
-#            elif "/" in data[i]: # date
-#                label = parse_date(data,i,label)
-#            elif len(data[i]) >= 2: # loc
-#                try:
-#                    blur_pro = process.extractOne(data[i], loc_table)
-#                    if blur_pro[1] > 40:
-#                        loc_list.append(blur_pro[0])
-#                except:
-#                    print(data[i])
-#        if len(loc_list) == 2:
-#            label["loc_from"] = loc_list[0]
-#            label["loc_to"] = loc_list[1]
-#        else:
-#            label["Error"].append("loc_from")
-#            label["Error"].append("loc_to")
-#    elif any([("看" in d or "搜尋" in d  or "查詢" in d or "query" in d or "查" in d) for d in data]):
-#        label["type"]="query"
-#        for i in range(len(data)):
-#            if ":" in data[i]: # time
-#                label = parse_time(data,i,label)
-#            elif "/" in data[i]: # date
-#                label = parse_date(data,i,label)
-#            elif len(data[i]) >= 2: # loc
-#                try:
-#                    blur_pro = process.extractOne(data[i], loc_table)
-#                    if blur_pro[1] > 40:
-#                        label["loc"] = blur_pro[0]
-#                except:
-#                    label["Error"].append("loc")
-#                    print(data[i])
-#
-#
-#    return label
+    if options.nobot:
+        return None
+    data = ws([text])[0]
+
+    label = {"type":"","date":"","time":"","loc":"","loc_from":"","loc_to":"","Error":[]}
+    if any([("申報" in d or "report" in d) for d in data]):
+        label["type"] = "report"
+        loc_list = []
+        for i in range(len(data)):
+            if ":" in data[i]: # time
+                label = parse_time(data,i,label)
+            elif "/" in data[i]: # date
+                label = parse_date(data,i,label)
+            elif len(data[i]) >= 2: # loc
+                try:
+                    blur_pro = process.extractOne(data[i], loc_table)
+                    if blur_pro[1] > 40:
+                        loc_list.append(blur_pro[0])
+                except:
+                    print(data[i])
+        if len(loc_list) == 2:
+            label["loc_from"] = loc_list[0]
+            label["loc_to"] = loc_list[1]
+        else:
+            label["Error"].append("loc_from")
+            label["Error"].append("loc_to")
+    elif any([("看" in d or "搜尋" in d  or "查詢" in d or "query" in d or "查" in d) for d in data]):
+        label["type"]="query"
+        for i in range(len(data)):
+            if ":" in data[i]: # time
+                label = parse_time(data,i,label)
+            elif "/" in data[i]: # date
+                label = parse_date(data,i,label)
+            elif len(data[i]) >= 2: # loc
+                try:
+                    blur_pro = process.extractOne(data[i], loc_table)
+                    if blur_pro[1] > 40:
+                        label["loc"] = blur_pro[0]
+                except:
+                    label["Error"].append("loc")
+                    print(data[i])
+
+
+    return label
 
 def parse_date(data,i,label):
     if len(data[i])==1:
